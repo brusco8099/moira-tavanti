934ea55eb48ebfefcf422c0c0cf7c6db1f875eb5
brusco8099 <alessandro.bruni.848@gmail.com>
2025-01-20 18:44:07 +0100

    urlo del sium
    speriamo funzioni stavolta

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function reindirizza(target){
+    
+    if(target == 1){
+        window.location.href = "location1.html";
+    }
+    else if(target == 2){
+        window.location.href = "location2.html";
+    }
+    else if(target == 3){
+        window.location.href = "location3.html"
+    }
+    else{
+        window.location.href = "index.html";
+    }
+}
